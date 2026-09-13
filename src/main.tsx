bfab81ee925e4e9bdf1106ee94192cfe8807236f
TI-06 <iretaku.shop@gmail.com>
2026-09-14 07:24:19 +0900

    style: load match feedback styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import './styles/app.css';
 import './styles/match.css';
+import './styles/feedback.css';
 
 const root = document.getElementById('root');
 
