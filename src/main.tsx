dc708b893db43e78edf7b56b8fd557826d5ab475
TI-06 <iretaku.shop@gmail.com>
2026-09-16 14:45:04 +0900

    refactor: remove legacy style imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import './styles/app.css';
-import './styles/match.css';
-import './styles/rework-menu.css';
-import './styles/rework-match.css';
-import './styles/feedback.css';
-import './styles/orientation.css';
 import './styles/v3-match.css';
 
 const root = document.getElementById('root');
